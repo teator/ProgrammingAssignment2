5bd2116bc6ce605efafb6c0e368ca036f3220ad8
teator <teator@users.noreply.github.com>
2014-07-27 19:10:36 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-inv <- NULL
+    inv <- NULL
     set <- function(y){
         x <<- y
         inv <<- NULL
